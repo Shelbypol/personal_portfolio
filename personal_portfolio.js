55c5b26811cebb09248d172bf8ee2d1a2b6997a9
shelby <shelby@shelbys-air.lan>
2022-08-27 18:09:57 -0500

    multi line drawn from middle of page

--- personal_portfolio.js
+++ personal_portfolio.js
@@ -8,41 +8,42 @@
 
     var cursorX;
     var cursorY;
-    var vertices = [{x:0, y:0}]
+    var points = [{x:0, y:0}]
 
+    let count = 0;
     document.onclick = function(e){
+       
         cursorX = e.pageX;
         cursorY = e.pageY;
         console.log('cursorX', cursorX)
         console.log('cursorY', cursorY)
 
-
-
-        vertices.push( {x:cursorX, y:cursorY} );
-        animate(vertices)
-        
+        points.push( {x:cursorX, y:cursorY} );
+        if(count > 0){
+            animate(points, false)
+        }else{
+            animate(points, true)
+        }
+        count++;
     }
 
    
    
-function animate(points){
-     
-    // let last_x = points[points.length-1].x;
-    // let last_y = points[points.length-1].y;
+function animate(points, isFirstClick){
+     console.log('points', points)
+    let start_points_x =  points[points.length-2].x;
+    let start_points_y = points[points.length-2].y;
 
-        let start_points_x =  points[points.length-2].x;
-        let start_points_y = points[points.length-2].y;
-
-        let end_points_x = points[points.length-1].x
-        let end_points_y = points[points.length-1].y
-
-    //     drawLine(ctx, [last_x, last_y], [end_points_x, end_points_y], 'green', 5);
-    //     ctx.stroke();
+    let end_points_x = points[points.length-1].x
+    let end_points_y = points[points.length-1].y
        
     ctx.lineWidth = 5;
 
     // draw a red line
-    ctx.beginPath();
+    if(isFirstClick){
+        ctx.beginPath();
+    }
+    
     ctx.moveTo(start_points_x, start_points_y);
     ctx.moveTo(end_points_x, end_points_y);
     ctx.lineTo(300, 100);
